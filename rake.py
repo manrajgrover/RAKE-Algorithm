24a87889558dfc6ca8855343c3c2483b286419a3
Manraj Singh <manrajsinghgrover@gmail.com>
2014-10-02 22:38:32 +0530

    Word Score Calculated
    Implemented Word Score

--- rake.py
+++ rake.py
@@ -30,6 +30,26 @@ def get_phrases(sentences):
             phrase.append(w)
     return phrases
 
+def wordscore(phrases):
+    w_freq= Counter()
+    w_deg={}
+    p=[]
+    for phrase in phrases:
+        p.append(Counter(phrase))
+        deg = len(filter(lambda x: not isNum(x), phrase))
+        for w in phrase:
+            if(w_deg.has_key(w)):
+                w_deg[w]=w_deg[w]+deg
+            else:
+                w_deg[w]=deg
+    x=[]
+    for x in range(len(p)):
+        p[0]=p[0]+p[x]
+    score={}
+    for w in w_deg:
+        score[w]=round(Decimal(w_deg[w]),2)/round(Decimal(p[0][w]),2)
+    return score
+
 text="""Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types."""
 
 stopwords=open("stopwords.txt","r")
@@ -37,3 +57,4 @@ stopwords= stopwords.read()
 stopword_list=stopwords.split()
 sentences=nltk.sent_tokenize(text)
 phrases = get_phrases(sentences)
+sc=wordscore(phrases)
